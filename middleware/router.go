5343901b6c09c5f778ae6b7d9e020c48f6391658
arsen <shokshokshoki@mail.ru>
2017-11-22 13:09:40 +0500

    refatoring

--- middleware/router.go
+++ middleware/router.go
@@ -2,6 +2,9 @@ package middleware
 
 import "github.com/gorilla/mux"
 
+/*
+RegisterRoutes register routes and return router
+ */
 func RegisterRoutes() *mux.Router {
 	router := mux.NewRouter()
 	//router.HandleFunc()
--- server.go
+++ server.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"net/http"
 	"slurm_statistics/config"
+	"slurm_statistics/logging"
 	"slurm_statistics/middleware"
 	"slurm_statistics/utils"
-	"fmt"
-	"net/http"
-	log "github.com/sirupsen/logrus"
-	"slurm_statistics/logging"
 )
 
 func main() {
